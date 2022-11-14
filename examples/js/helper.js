43d319ffffb874ce14570e3f9c8c8530820b07cb
redhoshi <rosematrix0910o@gmail.com>
2022-11-14 17:00:22 +0900

    first demo

--- examples/js/helper.js
+++ examples/js/helper.js
@@ -3,18 +3,18 @@ function create_index(array){
     while (array.length > 0) {
         n = array.length;
         k = Math.floor(Math.random() * n);
-      
         newArray.push(array[k]);
         array.splice(k, 1);
     }
+    console.log('newarray2',newArray,array)
     return newArray
 }
 
 function shuffle_file(filename){
     newfile = [];
     var rand_array = [0, 1];
+    rand_array = create_index(rand_array)
     for(let i=0;i<filename.length;i++){
-        rand_array = create_index(rand_array)
         newfile[i] = [filename[i][rand_array[0]],filename[i][rand_array[1]]]
     }
     return newfile
